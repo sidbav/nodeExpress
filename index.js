6d820ff3ffbf131a35071178ac54e56acf0d2358
Sidharth Baveja <sbaveja@edu.uwaterloo.ca>
2018-07-14 09:18:58 -0400

    PUT and DELETE requests

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
+
+//mongoose connecttion
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/CRMdb'),
+
+//body parser setup
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 routes(app);
 
@@ -11,5 +21,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Your server   is running on port ${PORT}`);
+  console.log(`Your server is running on port ${PORT}`);
 });
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,21 +1,30 @@
+import {
+  addNewContact,
+  getContacts,
+  getContactWithID,
+  updateContact,
+  deleteContact,
+ } from '../controllers/crmController';
+
 const routes = (app) => {
-  app.route('/contact')
-    .get((req, res, next) =>{
-      //middleware (connects to repsonse and request)
-      console.log(`Request from: ${req.originalURL}`)
-      console.log(`Request type: ${req.method}`)
-      next();
-    }, (req, res, next) => {
-      res.send('GET request successful');
-    });
-    app.route('/contact').post((req, res) =>
-      res.send('POST request successful'));
+    app.route('/contact')
+    .get((req, res, next) => {
+        // middleware
+        console.log(`Request from: ${req.originalUrl}`)
+        console.log(`Request type: ${req.method}`)
+        next();
+    }, getContacts)
+
+    // POST endpoint
+    .post(addNewContact);
 
     app.route('/contact/:contactId')
-      .put((req, res) =>
-        res.send('PUT request successful'))
-      .delete((req, res) =>
-        res.send('DELETE request successful'));
+    // get specific contact
+    .get(getContactWithID)
+
+    .put(updateContact)
+
+    .delete(deleteContact);
 }
 
 export default routes;
